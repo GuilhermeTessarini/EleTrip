bbace449f48fbc30fc13bf35ef14d69313d4a504
Guilherme Tessarini <guilherme.tessarini@hotmail.com>
2024-03-04 14:26:59 -0300

    Readme update

--- Back-end/table.sql
+++ Back-end/table.sql
@@ -1,59 +1,62 @@
-create table usuario(
-    idUsuario INT PRIMARY KEY AUTO_INCREMENT,
-    nomeUsuario VARCHAR(255),
-    emailUsuario VARCHAR(255),
-    telefoneUsuario VARCHAR(15),
-    dataNascimentoUsuario DATE,
-    cpfUsuario VARCHAR(12),
-    rgUsuario VARCHAR(10),
-    generoUsuario INTEGER(11),
-    senhaUsuario VARCHAR(255),
-    cidadeUsuario VARCHAR(255),
-    estadoUsuario VARCHAR(50),
-    ruaUsuario VARCHAR(255),
-    numeroUsuario INTEGER(20),
-    bairroUsuario VARCHAR(255),
-    numeroRegistroHabilitacao INTEGER(30),
-    validadeHabilitacao DATE,
-    cnhHabilitacao INTEGER(30),
-    role VARCHAR(20),
+CREATE TABLE `carroceria` (
+  `idCarroceria` int(11) NOT NULL AUTO_INCREMENT,
+  `nomeCarroceria` varchar(255) NOT NULL,
+  PRIMARY KEY (`idCarroceria`),
+  UNIQUE KEY `nomeCarroceria` (`nomeCarroceria`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-    UNIQUE(cpfUsuario, rgUsuario, emailUsuario)
-);
+CREATE TABLE `fatura` (
+  `idFatura` int(11) NOT NULL AUTO_INCREMENT,
+  `uuidFatura` varchar(200) NOT NULL,
+  `nomeFatura` varchar(255) NOT NULL,
+  `emailFatura` varchar(255) NOT NULL,
+  `telefoneFatura` varchar(20) NOT NULL,
+  `metodoPagamentoFatura` varchar(50) NOT NULL,
+  `totalFatura` int(11) NOT NULL,
+  `veiculoDetalhe` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL CHECK (json_valid(`veiculoDetalhe`)),
+  `criadoPor` varchar(255) NOT NULL,
+  `dataRetirada` date DEFAULT NULL,
+  `dataRetorno` date DEFAULT NULL,
+  `status` varchar(20) NOT NULL,
+  PRIMARY KEY (`idFatura`)
+) ENGINE=InnoDB AUTO_INCREMENT=37 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-create table carroceria(
-    idCarroceria INT PRIMARY KEY AUTO_INCREMENT,
-    nomeCarroceria VARCHAR(255) NOT NULL ,
+CREATE TABLE `usuario` (
+  `idUsuario` int(11) NOT NULL AUTO_INCREMENT,
+  `nomeUsuario` varchar(255) NOT NULL,
+  `emailUsuario` varchar(255) NOT NULL,
+  `telefoneUsuario` varchar(15) NOT NULL,
+  `dataNascimentoUsuario` date NOT NULL,
+  `cpfUsuario` varchar(12) NOT NULL,
+  `rgUsuario` varchar(10) NOT NULL,
+  `generoUsuario` int(11) NOT NULL,
+  `senhaUsuario` varchar(255) NOT NULL,
+  `cidadeUsuario` varchar(255) NOT NULL,
+  `estadoUsuario` varchar(50) NOT NULL,
+  `ruaUsuario` varchar(255) NOT NULL,
+  `numeroUsuario` int(20) NOT NULL,
+  `bairroUsuario` varchar(255) NOT NULL,
+  `numeroRegistroHabilitacao` varchar(25) NOT NULL,
+  `validadeHabilitacao` date NOT NULL,
+  `cnhHabilitacao` varchar(25) NOT NULL,
+  `role` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`idUsuario`),
+  UNIQUE KEY `cpfUsuario` (`cpfUsuario`,`rgUsuario`,`emailUsuario`)
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-    UNIQUE(nomeCarroceria)
-);
-
-create table veiculo(
-    idVeiculo INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    nomeVeiculo VARCHAR(255) NOT NULL,
-    anoVeiculo INTEGER(4) NOT NULL,
-    autonomiaVeiculo INTEGER(4) NOT NULL,
-    marcaVeiculo VARCHAR(255) NOT NULL,
-    passageirosVeiculo INTEGER(4) NOT NULL,
-    placaVeiculo VARCHAR(50) NOT NULL,
-    corVeiculo VARCHAR(255) NOT NULL,
-    motorVeiculo VARCHAR(255) NOT NULL,
-    valorLocacao INTEGER(100) NOT NULL,
-    dataLocacao DATE NOT NULL,
-    dataDevolucaoLocacao DATE NOT NULL,
-    carroceriaId INT NOT NULL,
-    imagemVeiculo BLOB NOT NULL,
-    statusVeiculo VARCHAR(255)
-);
-
-create table fatura(
-    idFatura INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    uuidFatura VARCHAR(200) NOT NULL,
-    nomeFatura VARCHAR(255) NOT NULL,
-    emailFatura VARCHAR(255) NOT NULL,
-    telefoneFatura VARCHAR (20) NOT NULL,
-    metodoPagamentoFatura VARCHAR(50) NOT NULL,
-    totalFatura INT NOT NULL,
-    veiculoDetalhe JSON DEFAULT NULL,
-    criadoPor VARCHAR(255) NOT NULL
-);
+CREATE TABLE `veiculo` (
+  `idVeiculo` int(11) NOT NULL AUTO_INCREMENT,
+  `nomeVeiculo` varchar(255) NOT NULL,
+  `anoVeiculo` int(4) NOT NULL,
+  `autonomiaVeiculo` int(4) NOT NULL,
+  `marcaVeiculo` varchar(255) NOT NULL,
+  `passageirosVeiculo` int(4) NOT NULL,
+  `placaVeiculo` varchar(50) NOT NULL,
+  `corVeiculo` varchar(255) NOT NULL,
+  `motorVeiculo` varchar(255) NOT NULL,
+  `valorLocacao` int(100) NOT NULL,
+  `carroceriaId` int(11) NOT NULL,
+  `imagemVeiculo` varchar(255) NOT NULL,
+  `statusVeiculo` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`idVeiculo`)
+) ENGINE=InnoDB AUTO_INCREMENT=43 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
--- README.md
+++ README.md
@@ -1,1 +1,51 @@
-# EletTrip
+# EleTrip - EN
+
+## Description
+
+EleTrip is my graduation project. It is a web application developed to facilitate the management and rental of electric cars. Our goal is to promote sustainable mobility by providing users with an efficient and ecological alternative for their transportation needs.
+
+## Key Features
+
+- **User Registration:** Register and create your account on EleTrip.
+- **Easy Reservations:** Make reservations quickly and easily.
+- **Vehicle Details:** Get detailed information about each car, including range, features, and price.
+- **Vehicle Management:** View, modify, or delete existing vehicles.
+
+## Tecnologias Utilizadas
+
+- **Angular:** Front end of the application.
+- **Node.js:** Back end for business logic.
+- **MySQL:** Database for storing information.
+
+## Author
+Guilherme Tessarini de Lima
+
+## Supervisor
+David Buzatto
+
+# EleTrip - PT-BR
+
+## Descrição
+
+O EleTrip é o meu projeto de conclusão de curso (TCC), ele é um aplicativo web desenvolvido para facilitar a gestão e aluguel de carros elétricos. Nosso objetivo é promover a mobilidade sustentável, oferecendo aos usuários uma alternativa eficiente e ecológica para suas necessidades de transporte.
+
+## Funcionalidades Principais
+
+- **Cadastro de Usuários:** Registre-se e crie sua conta no EleTrip.
+- **Reserva Simples:** Faça reservas de forma fácil e rápida.
+- **Detalhes do Veículo:** Obtenha informações detalhadas sobre cada carro, incluindo autonomia, recursos e preço.
+- **Gestão de Veículos:** Visualize, modifique ou exclua os veículos existentes.
+
+## Tecnologias Utilizadas
+
+- **Angular:** Front end do aplicativo.
+- **Node.js:** Back end para a lógica de negócios.
+- **MySQL:** Banco de dados para armazenamento de informações.
+
+## Autor
+Guilherme Tessarini de Lima
+
+## Orientador
+David Buzatto
+
+
